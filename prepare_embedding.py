e231d4c683583353bd1cd557eaf0ffedf213047a
Manash Kumar Mandal <manash@protonmail.com>
2018-04-18 13:39:43 +0600

    Update prepare_embedding.py

--- prepare_embedding.py
+++ prepare_embedding.py
@@ -1,11 +1,15 @@
 import numpy as np
+from gensim.models import KeyedVectors
+
+w2vembedding = KeyedVectors.load('embedding.vec')
+
 
 error_words = []
 embedding_matrix = np.zeros((NUM_TOKENS + 1, EMBEDDING_DIM))
 error = 0
 for idx, word in tqdm(enumerate(vocabulary)):
     try:
-        embedding_matrix[idx + 1] = glove_embeddings[vocabulary[idx]]
+        embedding_matrix[idx + 1] = w2vembedding[vocabulary[idx]]
     except:
         error_words.append(vocabulary[idx])
         error +=1
